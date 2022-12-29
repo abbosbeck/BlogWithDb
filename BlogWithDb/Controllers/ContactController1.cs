1234c251a7837e02feedd0fad418b90b81fe8d77
Abbosdev <AbbosAlisherovich2003@gmail.com>
2022-12-29 12:17:48 +0500

    added contactController and view

---
+++ BlogWithDb/Controllers/ContactController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogWithDb.Controllers
+{
+    public class ContactController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
